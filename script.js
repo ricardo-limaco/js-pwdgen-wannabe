1ae2c5806144b0158cb7f8fd91801d27cc256160
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-07 13:21:41 +0200

    JS Completed

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const nomeUtente = prompt("Inserisci il tuo Nome");
+const cognomeUtente = prompt("Inserisci il tuo Cognome");
+const coloreUtente = prompt("Inserisci il tuo Colore Preferito");
+const numero = 21;
+
+
+console.log("Nome utente", nomeUtente);
+console.log("Cognome utente", cognomeUtente);
+console.log("Colore preferito utente", coloreUtente);
+
+
+alert(nomeUtente + "-" + cognomeUtente + "-" + coloreUtente + "-" + numero);
